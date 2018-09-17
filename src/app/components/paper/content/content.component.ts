6f4e84570aa1be08a79fd257d90c0ec6383f1a0c
Hayden Rahn <anhayzzing33@gmail.com>
2018-09-17 08:12:45 -0500

    done

--- src/app/components/paper/content/content.component.ts
+++ src/app/components/paper/content/content.component.ts
@@ -9,14 +9,19 @@ import { ActivatedRoute } from "@angular/router";
 export class ContentComponent implements OnInit {
   public color: string;
   private sub: any;
+  public img : number;
   constructor(private route: ActivatedRoute) {}
   ngOnInit() {
+    this.img=0;
     this.sub = this.route.params.subscribe(params => {
       this.color = params["color"];
       console.log(this.color);
     });
   }
-
+  changeImg(num){
+    this.img = num;
+    console.log(this.img)
+  }
   ngOnDestroy() {
     this.sub.unsubscribe;
   }
