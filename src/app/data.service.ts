f62d57beaf3eba94c9b7985d3d26d5bfb03f696d
Hayden Rahn <anhayzzing33@gmail.com>
2018-09-18 23:41:49 -0500

    nito

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -16,10 +16,10 @@ export class DataService {
   titles ={
     "red":"Skills",
     "yellow":"About me",
-    "green":"projects <Ilgi>",
-    "lightgreen":"projects <EB-bed>",
-    "skyblue":"projects <Sacks Roofing>",
-    "navy":"projects <Nito>",
+    "green":"Ilgi",
+    "lightgreen":"EB health care",
+    "skyblue":"Sack's",
+    "navy":"Nito",
     "blue":"contact",
     "purple":"language"
   }
